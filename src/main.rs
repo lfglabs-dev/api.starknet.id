b114755ccbbb6db68a5c1bcf4970d3b820b3eb77
Thomas Marchand <thomas.marchand@tuta.io>
2023-05-19 17:43:54 +0200

    feat: add id_to_infts

--- src/main.rs
+++ src/main.rs
@@ -66,6 +66,7 @@ async fn main() {
         .route("/data_to_ids", get(endpoints::data_to_ids::handler))
         .route("/domain_to_addr", get(endpoints::domain_to_addr::handler))
         .route("/id_to_data", get(endpoints::id_to_data::handler))
+        .route("/id_to_infts", get(endpoints::id_to_infts::handler))
         .with_state(shared_state)
         .layer(cors);
 
