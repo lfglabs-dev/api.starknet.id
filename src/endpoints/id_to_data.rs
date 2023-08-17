a202fce88123dcd4d81ed08d21611b01c33cf3eb
Thomas Marchand <thomas.marchand@tuta.io>
2023-08-17 09:03:00 +0100

    feat: add old verifier and starknet_id in id_to_data (#10)

--- src/endpoints/id_to_data.rs
+++ src/endpoints/id_to_data.rs
@@ -1,4 +1,7 @@
-use crate::{models::AppState, utils::get_error};
+use crate::{
+    models::{AppState, Data},
+    utils::get_error,
+};
 use axum::{
     extract::{Query, State},
     http::{HeaderMap, HeaderValue, StatusCode},
@@ -6,25 +9,8 @@ use axum::{
 };
 use futures::StreamExt;
 use mongodb::bson::{doc, Document};
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use std::sync::Arc;
-
-#[derive(Serialize)]
-pub struct Data {
-    domain: String,
-    addr: Option<String>,
-    domain_expiry: Option<i32>,
-    is_owner_main: bool,
-    owner_addr: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    github: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    twitter: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    discord: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    proof_of_personhood: Option<String>,
-}
 
 #[derive(Deserialize)]
 pub struct IdQuery {
@@ -98,7 +84,7 @@ pub async fn handler(
                             // utf-8 encoded: github, twitter, discord
                             "$in": ["113702622229858", "32782392107492722", "28263441981469284"]
                         },
-                        "verifier": &state.conf.contracts.verifier.to_string()
+                        "verifier": { "$in": [ &state.conf.contracts.verifier.to_string(), &state.conf.contracts.old_verifier.to_string() ] }
                     },
                     {
                         // utf-8 encoded: proof_of_personhood
@@ -112,7 +98,7 @@ pub async fn handler(
         },
         doc! {
             "$group": {
-                "_id": "$field",
+                "_id": { "field": "$field", "verifier": "$verifier" },
                 "data": { "$first": "$data" }
             }
         },
@@ -125,15 +111,50 @@ pub async fn handler(
     let mut twitter = None;
     let mut discord = None;
     let mut proof_of_personhood = None;
+    let mut old_github = None;
+    let mut old_twitter = None;
+    let mut old_discord = None;
 
     if let Ok(mut cursor) = results {
         while let Some(result) = cursor.next().await {
             if let Ok(doc) = result {
-                match doc.get_str("_id") {
-                    Ok("113702622229858") => github = doc.get_str("data").ok().map(String::from),
-                    Ok("32782392107492722") => twitter = doc.get_str("data").ok().map(String::from),
-                    Ok("28263441981469284") => discord = doc.get_str("data").ok().map(String::from),
-                    Ok("2507652182250236150756610039180649816461897572") => {
+                let field = doc
+                    .get_document("_id")
+                    .unwrap()
+                    .get_str("field")
+                    .unwrap_or_default();
+                let verifier = doc
+                    .get_document("_id")
+                    .unwrap()
+                    .get_str("verifier")
+                    .unwrap_or_default();
+                // it's a bit ugly but it will get better when we removed the old verifier
+                match (field, verifier) {
+                    ("113702622229858", v) if v == state.conf.contracts.verifier.to_string() => {
+                        github = doc.get_str("data").ok().map(String::from)
+                    }
+                    ("113702622229858", v)
+                        if v == state.conf.contracts.old_verifier.to_string() =>
+                    {
+                        old_github = doc.get_str("data").ok().map(String::from)
+                    }
+                    ("32782392107492722", v) if v == state.conf.contracts.verifier.to_string() => {
+                        twitter = doc.get_str("data").ok().map(String::from)
+                    }
+                    ("32782392107492722", v)
+                        if v == state.conf.contracts.old_verifier.to_string() =>
+                    {
+                        old_twitter = doc.get_str("data").ok().map(String::from)
+                    }
+                    ("28263441981469284", v) if v == state.conf.contracts.verifier.to_string() => {
+                        discord = doc.get_str("data").ok().map(String::from)
+                    }
+                    ("28263441981469284", v)
+                        if v == state.conf.contracts.old_verifier.to_string() =>
+                    {
+                        old_discord = doc.get_str("data").ok().map(String::from)
+                    }
+                    ("2507652182250236150756610039180649816461897572", _) => {
                         proof_of_personhood = doc.get_str("data").ok().map(String::from)
                     }
                     _ => {}
@@ -164,6 +185,10 @@ pub async fn handler(
         twitter,
         discord,
         proof_of_personhood,
+        old_github,
+        old_twitter,
+        old_discord,
+        starknet_id: query.id,
     };
 
     (StatusCode::OK, headers, Json(data)).into_response()
