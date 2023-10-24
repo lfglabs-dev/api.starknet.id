a0bc4db728a14eed1e87bfdab550d944caf77844
Thomas Marchand <thomas.marchand@tuta.io>
2023-10-24 20:50:01 +0700

    feat: add pp data to addr_to_full_ids

--- src/endpoints/addr_to_full_ids.rs
+++ src/endpoints/addr_to_full_ids.rs
@@ -24,6 +24,14 @@ pub struct FullId {
     domain: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     domain_expiry: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    nft_pp: Option<NFTPP>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct NFTPP {
+    contract: String,
+    id: String,
 }
 
 #[derive(Deserialize)]
@@ -40,41 +48,91 @@ pub async fn handler(
     State(state): State<Arc<AppState>>,
     Query(query): Query<AddrQuery>,
 ) -> impl IntoResponse {
-    let id_owners = state.starknetid_db.collection::<mongodb::bson::Document>("id_owners");
+    let id_owners = state
+        .starknetid_db
+        .collection::<mongodb::bson::Document>("id_owners");
 
-    let pipeline = vec![
+    let pipeline = [
         doc! {
-            "$match": {
-                "owner": to_hex(&query.addr),
+            "$addFields": doc! {
+                "id": to_hex(&query.addr),
                 "_cursor.to": Bson::Null
-            },
+            }
         },
         doc! {
-            "$lookup": {
+            "$lookup": doc! {
                 "from": "domains",
-                "let": { "local_id": "$id" },
+                "let": doc! {
+                    "local_id": "$id"
+                },
                 "pipeline": [
-                    { "$match": {
-                        "$expr": { "$eq": [ "$id", "$$local_id" ] },
-                        "_cursor.to": Bson::Null
-                    }}
+                    doc! {
+                        "$match": doc! {
+                            "$expr": doc! {
+                                "$eq": [
+                                    "$id",
+                                    "$$local_id"
+                                ]
+                            },
+                            "_cursor.to": Bson::Null
+                        }
+                    }
                 ],
-                "as": "domainData",
-            },
+                "as": "domainData"
+            }
         },
         doc! {
-            "$unwind": {
+            "$unwind": doc! {
                 "path": "$domainData",
-                "preserveNullAndEmptyArrays": true,
-            },
+                "preserveNullAndEmptyArrays": true
+            }
         },
         doc! {
-            "$project": {
+            "$lookup": doc! {
+                "from": "id_verifier_data",
+                "let": doc! {
+                    "local_id": "$id"
+                },
+                "pipeline": [
+                    doc! {
+                        "$match": doc! {
+                            "$expr": doc! {
+                                "$eq": [
+                                    "$id",
+                                    "$$local_id"
+                                ]
+                            },
+                            "field": doc! {
+                                "$in": [
+                                    // nft_pp_contract
+                                    "0x00000000000000000000000000000000006e66745f70705f636f6e7472616374",
+                                    // nft_pp_id
+                                    "0x0000000000000000000000000000000000000000000000000074776974746572"
+                                ]
+                            },
+                            "verifier": to_hex(&state.conf.contracts.pp_verifier),
+                            "_cursor.to": Bson::Null
+                        }
+                    },
+                    doc! {
+                        "$project": doc! {
+                            "_id": 0,
+                            "field": 1,
+                            "data": 1
+                        }
+                    }
+                ],
+                "as": "verifierData"
+            }
+        },
+        doc! {
+            "$project": doc! {
                 "_id": 0,
                 "id": 1,
                 "domain": "$domainData.domain",
                 "domain_expiry": "$domainData.expiry",
-            },
+                "pp_verifier_data": "$verifierData"
+            }
         },
     ];
 
@@ -93,10 +151,32 @@ pub async fn handler(
                     .to_string();
                     let domain = doc.get_str("domain").ok().map(String::from);
                     let domain_expiry = doc.get_i32("domain_expiry").ok();
+                    let pp_verifier_data = doc.get_array("pp_verifier_data").ok();
+                    let mut nft_pp = None;
+                    if let Some(data) = pp_verifier_data {
+                        if data.len() >= 2 {
+                            if let (Some(contract_data), Some(id_data)) = (data.get(0), data.get(1))
+                            {
+                                if let (Bson::Document(contract_doc), Bson::Document(id_doc)) =
+                                    (contract_data, id_data)
+                                {
+                                    if let (Ok(contract_str), Ok(id_str)) =
+                                        (contract_doc.get_str("data"), id_doc.get_str("data"))
+                                    {
+                                        nft_pp = Some(NFTPP {
+                                            contract: contract_str.to_string(),
+                                            id: id_str.to_string(),
+                                        });
+                                    }
+                                }
+                            }
+                        }
+                    }
                     full_ids.push(FullId {
                         id,
                         domain,
                         domain_expiry,
+                        nft_pp,
                     });
                 }
             }
